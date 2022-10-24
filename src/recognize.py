a6d3e67b5e0f97c573ea15fefaad314820976de7
yuanjs-0108 <13767589902@168.com>
2022-10-24 23:42:53 +0800

    "feat: add translation model and anser image auto cutting"

--- src/recognize.py
+++ src/recognize.py
@@ -3,13 +3,14 @@ from paddleocr import PaddleOCR,draw_ocr
 from PIL import Image
 import os
 import logging
+import translators as trans
 
 logging.basicConfig(filename='log/log.log',level=logging.DEBUG)
-img_path = './imgs/img_04.jpg'
+# logging.basicConfig(level=logging.DEBUG)
+img_path = './imgs/img_08.jpg'
 image = Image.open(img_path).convert('RGB')
-temp_img = 'cut.jpg'
 
-def rec(image_name):
+def rec_jp(image_name):
     ocr = PaddleOCR(use_angle_cls=True,lang='japan')
     # need a file not instance
     set = ocr.ocr(image_name,cls=True)
@@ -20,23 +21,75 @@ def rec(image_name):
     # [logging.debug(line) for line in result]
     # boxes = [line[0] for line in result]
     txts = [line[1][0] for line in result]
-    logging.debug(txts)
+    # logging.debug(txts)
+    return txts
     # scores = [line[1][1] for line in result]
 
+def jp_2_zh(txts):
+    if len(txts) == 0:
+        logging.error(f"NON Result")
+    jp_to_zh = trans.baidu(txts[0],from_language='jp',to_language='zh')
+    return jp_to_zh
 
-def cut_img(image:Image):
-    box = (100,400,980,550)
+def zh_2_jp(txts):
+    if len(txts) == 0:
+        logging.error(f"NON Result")
+    zh_to_jp = trans.baidu(txts[0],from_language='zh',to_language='jp')
+    return zh_to_jp
+
+def rec_zh(image_name):
+    ocr = PaddleOCR(use_angle_cls=True,lang='ch')
+    set = ocr.ocr(image_name,cls=True)
+    result = set[0]
+    # data = set[0][0][1][0]
+    logging.info(date.today().strftime("%b-%d-%Y"))
+    logging.info(f'[-]result: {len(result)}')
+    # [logging.debug(line) for line in result]
+    # boxes = [line[0] for line in result]
+    txts = [line[1][0] for line in result]
+    # logging.debug(txts)
+    return txts
+
+def qus_img(image:Image):
+    temp_img = 'cut.jpg'
+    # box = (100,400,980,550)
+    width,hight= image.size
+    # logging.debug(f'({width},{hight})')
+    box = (width * 0.1,hight * 0.13,width * 0.9,hight * 0.25)
     cut = image.crop(box)
+    # cut.show()
     cut.save(temp_img)
-    rec(temp_img)
+    qustion = rec_jp(temp_img)
+    result = jp_2_zh(qustion)
+    logging.debug(f'{temp_img}:{qustion} to {result}')
     os.remove(temp_img)
-    return cut
-# dir = [{key,val} for key,val in result]
+    return qustion
 
 def answer_image(image:image):
-    pass
+    width,hight= image.size
+    # logging.debug(f'({width},{hight})')
+    for setp in range(1,5):
+        box = (width * 0.1,hight * (0.32+(setp + setp * 0.1)/10), width * 0.9,hight * (0.45+(setp + setp * 0.1)/10))
+        cut = image.crop(box)
+        txt = f'temp-{setp}.jpg'
+        cut.save(txt)
+        context =  rec_jp(txt)
+        result = jp_2_zh(context)
+        logging.debug(f'{txt}:{context} trans to {result}')
+        os.remove(txt)
+        # cut.show()
+    # cut.save('temp.jpg')
+    # txt = rec_zh('temp.jpg')
+    # logging.debug(f'{txt}')
+    # os.remove('temp.jpg')
+    # rec_zh(image)
 
-cut_img(image)
+qus_img(image)
+answer_image(image)
+# org_txt = cut_img(image)
+# result = jp_2_zh(org_txt)
+# logging.debug(f"{org_txt} to {result}")
+# logging.debug(image.size)
 
 # image_name = f'{data}.jpg'
 # im_show = draw_ocr(image, boxes, txts, scores, font_path='./fonts/simfang.ttf')
